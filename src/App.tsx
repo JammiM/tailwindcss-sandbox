130fe307f2a074a399284c4445485521b77e5a6b
James Morrissey <jammim19@gmail.com>
2025-12-09 15:33:23 +0000

    Putting each component within it's div

--- src/App.tsx
+++ src/App.tsx
@@ -20,24 +20,30 @@ function App() {
       </div>
       <h1>Vite + React</h1> */}
 
-      <div className=" mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <img
-          className="size-12 shrink-0 md:w-40 lg:w-10"
-          src={chitChatIcon}
-          alt="ChitChat Logo"
-        />
+      <section>
         <div>
-          <div className=" text-xl font-medium text-black dark:text-white">
-            ChitChat
+          <div className=" mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+            <img
+              className="size-12 shrink-0 md:w-40 lg:w-10"
+              src={chitChatIcon}
+              alt="ChitChat Logo"
+            />
+            <div>
+              <div className=" text-xl font-medium text-black dark:text-white">
+                ChitChat
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">
+                You have a new message!
+                <a href="https://tailwindcss.com/docs/responsive-design">
+                  Read here
+                </a>
+              </p>
+            </div>
           </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            You have a new message!
-            <a href="https://tailwindcss.com/docs/responsive-design">
-              Read here
-            </a>
-          </p>
         </div>
-      </div>
+
+        <div></div>
+      </section>
 
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
